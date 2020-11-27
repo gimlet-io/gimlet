58a947b2d26498473c07c68259f3d049ba49d413
Laszlo Fogas <laszlo@laszlo.cloud>
2020-11-27 13:24:02 +0100

    Prepping stdin for seal test

--- commands/chart/chart_seal_test.go
+++ commands/chart/chart_seal_test.go
@@ -3,6 +3,8 @@ package chart
 import (
 	"github.com/franela/goblin"
 	"github.com/gimlet-io/gimlet-cli/commands"
+	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 )
@@ -16,7 +18,6 @@ func Test_seal(t *testing.T) {
 
 	g.Describe("gimlet chart seal", func() {
 		g.It("Should seal stdin", func() {
-
 			const toSeal = `
 key: value
 another: one
@@ -26,7 +27,19 @@ sealedSecrets:
   secret2: value2
 `
 
-			err := commands.Run(&Command, args)
+			tmpFile, err := ioutil.TempFile("", "dummyStdIn")
+			g.Assert(err == nil).IsTrue()
+			defer os.Remove(tmpFile.Name())
+			_, err = tmpFile.Write([]byte(toSeal))
+			g.Assert(err == nil).IsTrue()
+			_, err = tmpFile.Seek(0, 0)
+			g.Assert(err == nil).IsTrue()
+			oldStdin := os.Stdin
+			defer func() { os.Stdin = oldStdin }()
+
+			os.Stdin = tmpFile
+
+			err = commands.Run(&Command, args)
 			g.Assert(err == nil).IsTrue(err)
 		})
 	})
