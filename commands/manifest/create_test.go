21dd171d192d547087792aab862644ee2484225c
Laszlo Fogas <laszlo@laszlo.cloud>
2021-02-12 14:20:36 +0100

    tests

---
+++ commands/manifest/create_test.go
@@ -0,0 +1,68 @@
+package manifest
+
+import (
+	"github.com/franela/goblin"
+	"github.com/gimlet-io/gimlet-cli/commands"
+	"github.com/gimlet-io/gimletd/dx"
+	"io/ioutil"
+	"os"
+	"sigs.k8s.io/yaml"
+	"strings"
+	"testing"
+)
+
+func Test_create(t *testing.T) {
+	g := goblin.Goblin(t)
+	g.Describe("gimlet manifest create", func() {
+		g.It("Should resolve <repo>/<chart> format chart names from local repo", func() {
+			createdManifestPath, err := ioutil.TempFile("", "gimlet-cli-test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.Remove(createdManifestPath.Name())
+
+			//TODO set up the onechart repo with helm add in this test
+
+			args := strings.Split("gimlet manifest create", " ")
+			args = append(args, "--env", "staging")
+			args = append(args, "--app", "my-app")
+			args = append(args, "--namespace", "staging")
+			args = append(args, "--chart", "onechart/onechart")
+			args = append(args, "-o", createdManifestPath.Name())
+
+			err = commands.Run(&Command, args)
+			g.Assert(err == nil).IsTrue(err)
+			manifestString, err := ioutil.ReadFile(createdManifestPath.Name())
+			g.Assert(err == nil).IsTrue(err)
+
+			var m dx.Manifest
+			err = yaml.Unmarshal(manifestString, &m)
+			g.Assert(err == nil).IsTrue(err)
+			g.Assert(m.Chart.Repository == "https://chart.onechart.dev").IsTrue("Should resolve chart repo url")
+		})
+		g.It("Should resolve git@github.com:gimlet-io/onechart.git format chart names from git", func() {
+			createdManifestPath, err := ioutil.TempFile("", "gimlet-cli-test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.Remove(createdManifestPath.Name())
+
+			args := strings.Split("gimlet manifest create", " ")
+			args = append(args, "--env", "staging")
+			args = append(args, "--app", "my-app")
+			args = append(args, "--namespace", "staging")
+			args = append(args, "--chart", "git@github.com:gimlet-io/onechart.git?path=/charts/onechart/")
+			args = append(args, "-o", createdManifestPath.Name())
+
+			err = commands.Run(&Command, args)
+			g.Assert(err == nil).IsTrue(err)
+			manifestString, err := ioutil.ReadFile(createdManifestPath.Name())
+			g.Assert(err == nil).IsTrue(err)
+
+			var m dx.Manifest
+			err = yaml.Unmarshal(manifestString, &m)
+			g.Assert(err == nil).IsTrue(err)
+			g.Assert(m.Chart.Repository == "https://chart.onechart.dev").IsTrue("Should resolve chart repo url")
+		})
+	})
+}
