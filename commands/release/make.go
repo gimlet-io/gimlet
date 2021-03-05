8ad492a71ff37986d5664cd72b9a4ec3fd8b72fc
Laszlo Fogas <laszlo@laszlo.cloud>
2021-03-05 10:35:38 +0100

    Command for releasing an artifact

---
+++ commands/release/make.go
@@ -0,0 +1,73 @@
+package release
+
+import (
+	"fmt"
+	"github.com/enescakir/emoji"
+	"github.com/gimlet-io/gimletd/client"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/oauth2"
+	"os"
+)
+
+var releasemMakeCmd = cli.Command{
+	Name:  "make",
+	Usage: "Make an ad-hoc release",
+	UsageText: `gimlet release make \
+     --env staging \
+     --artifact an-artifact-id \
+     --server http://gimletd.mycompany.com
+     --token c012367f6e6f71de17ae4c6a7baac2e9`,
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:     "server",
+			Usage:    "GimletD server URL, GIMLET_SERVER environment variable alternatively",
+			EnvVars:  []string{"GIMLET_SERVER"},
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "token",
+			Usage:    "GimletD server api token, GIMLET_TOKEN environment variable alternatively",
+			EnvVars:  []string{"GIMLET_TOKEN"},
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "env",
+			Usage:    "make a release to this environment",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "artifact",
+			Usage:    "the artifact to release",
+			Aliases:  []string{"a"},
+			Required: true,
+		},
+	},
+	Action: list,
+}
+
+func make(c *cli.Context) error {
+	serverURL := c.String("server")
+	token := c.String("token")
+
+	config := new(oauth2.Config)
+	auth := config.Client(
+		oauth2.NoContext,
+		&oauth2.Token{
+			AccessToken: token,
+		},
+	)
+
+	client := client.NewClient(serverURL, auth)
+
+	err := client.ReleasesPost(
+		c.String("env"),
+		c.String("artifact"),
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(os.Stderr, "%v Release is now added to the release queue\n\n", emoji.WomanGesturingOk)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/fluxcd/flux2 v0.7.7
 	github.com/fluxcd/pkg/ssh v0.0.5
 	github.com/franela/goblin v0.0.0-20200105215937-c9ffbefa60db
-	github.com/gimlet-io/gimletd v0.0.0-20210303144950-d649e3aa97db
+	github.com/gimlet-io/gimletd v0.0.0-20210305093021-125e592a40a1
 	github.com/go-chi/chi v1.5.1
 	github.com/go-chi/cors v1.1.1
 	github.com/go-git/go-git/v5 v5.2.0
