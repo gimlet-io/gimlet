f4f4fdf424dc13ae49321462ab027a2a7b475f49
szabogabor91 <91szabogabor@gmail.com>
2022-02-18 14:52:46 +0100

    Fix DOM property warnings coming from stroke-linecap, stroke-linejoin and stroke-width

--- web/dashboard/src/views/services/services.jsx
+++ web/dashboard/src/views/services/services.jsx
@@ -117,7 +117,7 @@ export function emptyStateNoAgents() {
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
               d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z"/>
       </svg>
       <h3 className="mt-2 text-sm font-medium text-gray-900">No connected Gimlet Agents</h3>
