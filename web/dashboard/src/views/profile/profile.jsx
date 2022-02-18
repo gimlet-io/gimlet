f1704e2fc7eb17b99a11f3cf3cf8db736c9062ca
szabogabor91 <86593865+szabogabor91@users.noreply.github.com>
2022-02-18 19:11:08 +0100

    Fix DOM property warnings coming from stroke-linecap, stroke-linejoin and stroke-width (#86)

--- web/dashboard/src/views/profile/profile.jsx
+++ web/dashboard/src/views/profile/profile.jsx
@@ -133,7 +133,7 @@ function integrateGimletD() {
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
               d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
       </svg>
 
