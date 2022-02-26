812001fefd149e5ec58e8c2a4ff4a76d38457058
szabogabor91 <91szabogabor@gmail.com>
2022-02-26 15:34:58 +0100

    Fix text alignment of 'Separate environments by git repositories

--- web/dashboard/src/views/environments/environmentCard.jsx
+++ web/dashboard/src/views/environments/environmentCard.jsx
@@ -41,32 +41,28 @@ const EnvironmentCard = ({ isOnline, singleEnv, deleteEnv, hasGitopsRepo }) => {
             </div>
           </div>
         </div>
-        <div className=" text-gray-700">
-          <div>
-            <div className="sm:pt-5 flex">
-              <label htmlFor="username" className="block text-sm text-gray-700 sm:mt-px sm:pt-2 sm:col-span-2">
-                <div className="font-medium">Separate environments by git repositories</div>
-              </label>
-              <div className="mt-1 ml-4 sm:mt-0">
-                <div className="max-w-lg flex rounded-md mt-1 ml-4 sm:mt-0">
-                  <Switch
-                    checked={enabled}
-                    onChange={setEnabled}
+        <div className="text-gray-700">
+          <div className="sm:pt-5 flex">
+            <div className="font-medium">Separate environments by git repositories</div>
+            <div className="mt-1 ml-4 sm:mt-0">
+              <div className="max-w-lg flex rounded-md mt-1 ml-4 sm:mt-0">
+                <Switch
+                  checked={enabled}
+                  onChange={setEnabled}
+                  className={(
+                    enabled ? "bg-indigo-600" : "bg-gray-200") +
+                    " relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200"
+                  }
+                >
+                  <span className="sr-only">Use setting</span>
+                  <span
+                    aria-hidden="true"
                     className={(
-                      enabled ? "bg-indigo-600" : "bg-gray-200") +
-                      " relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200"
+                      enabled ? "translate-x-5" : "translate-x-0") +
+                      " pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
                     }
-                  >
-                    <span className="sr-only">Use setting</span>
-                    <span
-                      aria-hidden="true"
-                      className={(
-                        enabled ? "translate-x-5" : "translate-x-0") +
-                        " pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-                      }
-                    />
-                  </Switch>
-                </div>
+                  />
+                </Switch>
               </div>
             </div>
           </div>
