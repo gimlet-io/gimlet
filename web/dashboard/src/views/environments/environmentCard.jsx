9702c94e31e1dfc1cf3b71f56e1ef78a13be10f6
szabogabor91 <91szabogabor@gmail.com>
2022-02-26 11:50:05 +0100

    Change circle uninitialized icon to a triangle one and ml-1 to ml-2

--- web/dashboard/src/views/environments/environmentCard.jsx
+++ web/dashboard/src/views/environments/environmentCard.jsx
@@ -103,9 +103,9 @@ const EnvironmentCard = ({ isOnline, singleEnv, deleteEnv, hasGitopsRepo }) => {
               </svg>
             </span>
             {!hasGitopsRepo &&
-              <span title="Uninitiated">
-                <svg xmlns="http://www.w3.org/2000/svg" className="ml-1 h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              <span title="Uninitialized">
+                <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                 </svg>
               </span>}
           </div>
