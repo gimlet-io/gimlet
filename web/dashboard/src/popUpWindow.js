2ca3f4610e48c3adf5d5149c12107a1dca41425f
Ádám <sisakadam@gmail.com>
2022-03-07 15:43:51 +0100

    Outsourced inProgress, error and success popup messages to functions

--- web/dashboard/src/popUpWindow.js
+++ web/dashboard/src/popUpWindow.js
@@ -18,6 +18,59 @@ export default class PopUpWindow extends Component {
         });
     }
 
+    inProgress() {
+        return (
+            <div className="flex">
+                <div className="flex-shrink-0">
+                    <svg className="animate-spin h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth={4}></circle>
+                        <path className="opacity-75" fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                </div>
+                <div className="ml-3">
+                    <h3 className="text-sm font-medium text-gray-800">{this.state.popupWindow.header}</h3>
+                </div>
+            </div>)
+    }
+
+    error() {
+        return (
+            <div className="flex">
+                <div className="flex-shrink-0">
+                    <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+                </div>
+                <div className="ml-3">
+                    <h3 className="text-sm font-medium text-red-800">{this.state.popupWindow.header}</h3>
+                    <div className="mt-2 text-sm text-red-700">
+                        {this.state.popupWindow.errorList ?
+                            <ul className="list-disc pl-5 space-y-1">
+                                {this.renderErrorList()}
+                            </ul> :
+                            <p>{this.state.popupWindow.message}</p>
+                        }
+                    </div>
+                </div>
+            </div>)
+    }
+
+    success() {
+        return (
+            <div className="flex">
+                <div className="flex-shrink-0">
+                    <CheckIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
+                </div>
+                <div className="ml-3">
+                    <h3 className="text-sm font-medium text-green-800">{this.state.popupWindow.header}</h3>
+                    <div className="mt-2 text-sm text-green-700">
+                        <p>{this.state.popupWindow.message}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     backgroundColor() {
         return !this.state.popupWindow.finished ? "bg-gray-200" : this.state.popupWindow.isError ? "bg-red-50" : "bg-green-50"
     }
@@ -31,54 +84,11 @@ export default class PopUpWindow extends Component {
 
     savingText() {
         if (!this.state.popupWindow.finished) {
-            return (
-                <div className="flex">
-                    <div className="flex-shrink-0">
-                        <svg className="animate-spin h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24">
-                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth={4}></circle>
-                            <path className="opacity-75" fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                        </svg>
-                    </div>
-                    <div className="ml-3">
-                        <h3 className="text-sm font-medium text-gray-800">{this.state.popupWindow.header}</h3>
-                    </div>
-                </div>
-            )
+            return this.inProgress();
         } else if (this.state.popupWindow.isError) {
-            return (
-                <div className="flex">
-                    <div className="flex-shrink-0">
-                        <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
-                    </div>
-                    <div className="ml-3">
-                        <h3 className="text-sm font-medium text-red-800">{this.state.popupWindow.header}</h3>
-                        <div className="mt-2 text-sm text-red-700">
-                            {this.state.popupWindow.errorList ?
-                                <ul className="list-disc pl-5 space-y-1">
-                                    {this.renderErrorList()}
-                                </ul> :
-                                <p>{this.state.popupWindow.message}</p>
-                            }
-                        </div>
-                    </div>
-                </div>
-            )
+            return this.error();
         } else {
-            return (
-                <div className="flex">
-                    <div className="flex-shrink-0">
-                        <CheckIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
-                    </div>
-                    <div className="ml-3">
-                        <h3 className="text-sm font-medium text-green-800">{this.state.popupWindow.header}</h3>
-                        <div className="mt-2 text-sm text-green-700">
-                            <p>{this.state.popupWindow.message}</p>
-                        </div>
-                    </div>
-                </div>
-            )
+            return this.success();
         }
     }
 
