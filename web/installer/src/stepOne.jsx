da47362cde41735064c1ac022fe32b466c7c7997
Ádám <sisakadam@gmail.com>
2022-03-24 12:24:59 +0100

    Fixing HTML attributes for React

--- web/installer/src/stepOne.jsx
+++ web/installer/src/stepOne.jsx
@@ -29,88 +29,88 @@ const StepOne = () => {
     ]
   })
 
-    return (
-        <div class="mt-32 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="max-w-4xl mx-auto">
-          <div class="md:flex md:items-center md:justify-between">
-            <div class="flex-1 min-w-0">
-              <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate mb-12">Gimlet Installer</h2>
-            </div>
+  return (
+    <div className="mt-32 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto">
+        <div className="md:flex md:items-center md:justify-between">
+          <div className="flex-1 min-w-0">
+            <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate mb-12">Gimlet Installer</h2>
           </div>
-          <nav aria-label="Progress">
-            <ol class="border border-gray-300 rounded-md divide-y divide-gray-300 md:flex md:divide-y-0">
-              <li class="relative md:flex-1 md:flex">
-                {/* <!-- Current Step --> */}
-                <div class="px-6 py-4 flex items-center text-sm font-medium select-none cursor-default" aria-current="step">
-                  <span
-                    class="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-indigo-600 rounded-full">
-                    <span class="text-indigo-600">01</span>
-                  </span>
-                  <span class="ml-4 text-sm font-medium text-indigo-600">Create Github Application</span>
-                </div>
-  
-                {/* <!-- Arrow separator for lg screens and up --> */}
-                <div class="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
-                  <svg class="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
-                    <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor"
-                      stroke-linejoin="round" />
-                  </svg>
-                </div>
-              </li>
-  
-              <li class="relative md:flex-1 md:flex">
-                {/* <!-- Upcoming Step --> */}
-                <div class="group flex items-center select-none cursor-default">
-                  <span class="px-6 py-4 flex items-center text-sm font-medium">
-                    <span
-                      class="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-gray-300 rounded-full">
-                      <span class="text-gray-500">02</span>
-                    </span>
-                    <span class="ml-4 text-sm font-medium text-gray-500">Prepare gitops repository</span>
-                  </span>
-                </div>
-  
-                {/* <!-- Arrow separator for lg screens and up --> */}
-                <div class="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
-                  <svg class="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
-                    <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor"
-                      stroke-linejoin="round" />
-                  </svg>
-                </div>
-              </li>
-  
-              <li class="relative md:flex-1 md:flex">
-                {/* <!-- Upcoming Step --> */}
-                <div class="group flex items-center select-none cursor-default">
-                  <span class="px-6 py-4 flex items-center text-sm font-medium">
-                    <span
-                      class="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-gray-300 rounded-full">
-                      <span class="text-gray-500">03</span>
-                    </span>
-                    <span class="ml-4 text-sm font-medium text-gray-500">Bootstrap gitops automation</span>
-                  </span>
-                </div>
-              </li>
-            </ol>
-          </nav>
-  
-          <div class="mt-8 font-mono text-sm">
-            <p class="">Gimlet Dashboard uses a Github Application to gain access to your source code.</p>
-            <p class="">You must create this application first.</p>
-  
-            <p class="pt-8">Please note that this application will be owned by you, thus you don't give access to any third party or the makers of Gimlet.</p>
-  
-            <form action="https://github.com/settings/apps/new" method="post">
-              <input type="hidden" name="manifest" id="manifest" value={manifest}></input><br />
-              <input type="submit" value="Create Github app"
-                class="cursor-pointer font-sans inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              </input>
-            </form>
-          </div>
-  
         </div>
+        <nav aria-label="Progress">
+          <ol className="border border-gray-300 rounded-md divide-y divide-gray-300 md:flex md:divide-y-0">
+            <li className="relative md:flex-1 md:flex">
+              {/* <!-- Current Step --> */}
+              <div className="px-6 py-4 flex items-center text-sm font-medium select-none cursor-default" aria-current="step">
+                <span
+                  className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-indigo-600 rounded-full">
+                  <span className="text-indigo-600">01</span>
+                </span>
+                <span className="ml-4 text-sm font-medium text-indigo-600">Create Github Application</span>
+              </div>
+
+              {/* <!-- Arrow separator for lg screens and up --> */}
+              <div className="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
+                <svg className="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
+                  <path d="M0 -2L20 40L0 82" vectorEffect="non-scaling-stroke" stroke="currentcolor"
+                    strokeLinejoin="round" />
+                </svg>
+              </div>
+            </li>
+
+            <li className="relative md:flex-1 md:flex">
+              {/* <!-- Upcoming Step --> */}
+              <div className="group flex items-center select-none cursor-default">
+                <span className="px-6 py-4 flex items-center text-sm font-medium">
+                  <span
+                    className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-gray-300 rounded-full">
+                    <span className="text-gray-500">02</span>
+                  </span>
+                  <span className="ml-4 text-sm font-medium text-gray-500">Prepare gitops repository</span>
+                </span>
+              </div>
+
+              {/* <!-- Arrow separator for lg screens and up --> */}
+              <div className="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
+                <svg className="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
+                  <path d="M0 -2L20 40L0 82" vectorEffect="non-scaling-stroke" stroke="currentcolor"
+                    strokeLinejoin="round" />
+                </svg>
+              </div>
+            </li>
+
+            <li className="relative md:flex-1 md:flex">
+              {/* <!-- Upcoming Step --> */}
+              <div className="group flex items-center select-none cursor-default">
+                <span className="px-6 py-4 flex items-center text-sm font-medium">
+                  <span
+                    className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-gray-300 rounded-full">
+                    <span className="text-gray-500">03</span>
+                  </span>
+                  <span className="ml-4 text-sm font-medium text-gray-500">Bootstrap gitops automation</span>
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+
+        <div className="mt-8 font-mono text-sm">
+          <p className="">Gimlet Dashboard uses a Github Application to gain access to your source code.</p>
+          <p className="">You must create this application first.</p>
+
+          <p className="pt-8">Please note that this application will be owned by you, thus you don't give access to any third party or the makers of Gimlet.</p>
+
+          <form action="https://github.com/settings/apps/new" method="post">
+            <input type="hidden" name="manifest" id="manifest" value={manifest}></input><br />
+            <input type="submit" value="Create Github app"
+              className="cursor-pointer font-sans inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            </input>
+          </form>
+        </div>
+
       </div>
-    );
+    </div>
+  );
 };
 
 export default StepOne;
--- web/installer/src/stepThree.jsx
+++ web/installer/src/stepThree.jsx
@@ -6,145 +6,145 @@ const StepThree = ({ getContext }) => {
 
     useEffect(() => {
         getContext().then(data => setContext(data))
-            .catch(err => { console.error(err) });
-    }, [getContext])
-
-    console.log(context)
+            .catch(err => {
+                console.error(`Error: ${err}`);
+            });
+    }, [getContext]);
 
     if (!context) {
         return null;
     }
 
     return (
-        <div class="mt-32 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="max-w-4xl mx-auto">
-                <div class="md:flex md:items-center md:justify-between">
-                    <div class="flex-1 min-w-0">
-                        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate mb-12">Gimlet Installer</h2>
+        <div className="mt-32 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-4xl mx-auto">
+                <div className="md:flex md:items-center md:justify-between">
+                    <div className="flex-1 min-w-0">
+                        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate mb-12">Gimlet Installer</h2>
                     </div>
                 </div>
                 <nav aria-label="Progress">
-                    <ol class="border border-gray-300 rounded-md divide-y divide-gray-300 md:flex md:divide-y-0">
-                        <li class="relative md:flex-1 md:flex">
+                    <ol className="border border-gray-300 rounded-md divide-y divide-gray-300 md:flex md:divide-y-0">
+                        <li className="relative md:flex-1 md:flex">
                             {/* <!-- Completed Step --> */}
-                            <div class="group flex items-center w-full select-none cursor-default">
-                                <span class="px-6 py-4 flex items-center text-sm font-medium">
-                                    <span class="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full ">
+                            <div className="group flex items-center w-full select-none cursor-default">
+                                <span className="px-6 py-4 flex items-center text-sm font-medium">
+                                    <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full ">
                                         {/* <!-- Heroicon name: solid/check --> */}
-                                        <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                        <svg className="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                             fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd"
+                                            <path fillRule="evenodd"
                                                 d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                                clip-rule="evenodd" />
+                                                clipRule="evenodd" />
                                         </svg>
                                     </span>
-                                    <span class="ml-4 text-sm font-medium text-gray-900">Create Github Application</span>
+                                    <span className="ml-4 text-sm font-medium text-gray-900">Create Github Application</span>
                                 </span>
                             </div>
 
                             {/* <!-- Arrow separator for lg screens and up --> */}
-                            <div class="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
-                                <svg class="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
-                                    <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor"
-                                        stroke-linejoin="round" />
+                            <div className="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
+                                <svg className="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
+                                    <path d="M0 -2L20 40L0 82" vectorEffect="non-scaling-stroke" stroke="currentcolor"
+                                        strokeLinejoin="round" />
                                 </svg>
                             </div>
                         </li>
 
-                        <li class="relative md:flex-1 md:flex">
+                        <li className="relative md:flex-1 md:flex">
                             {/* <!-- Completed Step --> */}
-                            <div class="group flex items-center w-full select-none cursor-default">
-                                <span class="px-6 py-4 flex items-center text-sm font-medium">
-                                    <span class="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full ">
+                            <div className="group flex items-center w-full select-none cursor-default">
+                                <span className="px-6 py-4 flex items-center text-sm font-medium">
+                                    <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full ">
                                         {/* <!-- Heroicon name: solid/check --> */}
-                                        <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                        <svg className="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                             fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd"
+                                            <path fillRule="evenodd"
                                                 d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                                clip-rule="evenodd" />
+                                                clipRule="evenodd" />
                                         </svg>
                                     </span>
-                                    <span class="ml-4 text-sm font-medium text-gray-900">Prepare gitops repository</span>
+                                    <span className="ml-4 text-sm font-medium text-gray-900">Prepare gitops repository</span>
                                 </span>
                             </div>
 
                             {/* <!-- Arrow separator for lg screens and up --> */}
-                            <div class="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
-                                <svg class="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
-                                    <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor"
-                                        stroke-linejoin="round" />
+                            <div className="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
+                                <svg className="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
+                                    <path d="M0 -2L20 40L0 82" vectorEffect="non-scaling-stroke" stroke="currentcolor"
+                                        strokeLinejoin="round" />
                                 </svg>
                             </div>
                         </li>
 
-                        <li class="relative md:flex-1 md:flex">
+                        <li className="relative md:flex-1 md:flex">
                             {/* <!-- Current Step --> */}
-                            <div class="px-6 py-4 flex items-center text-sm font-medium select-none cursor-default" aria-current="step">
+                            <div className="px-6 py-4 flex items-center text-sm font-medium select-none cursor-default" aria-current="step">
                                 <span
-                                    class="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-indigo-600 rounded-full">
-                                    <span class="text-indigo-600">03</span>
+                                    className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-indigo-600 rounded-full">
+                                    <span className="text-indigo-600">03</span>
                                 </span>
-                                <span class="ml-4 text-sm font-medium text-indigo-600">Bootstrap gitops automation</span>
+                                <span className="ml-4 text-sm font-medium text-indigo-600">Bootstrap gitops automation</span>
                             </div>
                         </li>
                     </ol>
                 </nav>
 
                 {context.appId === "" ?
-                    (<div class="rounded-md bg-red-50 p-4 my-8">
-                        <div class="flex">
-                            <div class="flex-shrink-0">
+                    (<div className="rounded-md bg-red-50 p-4 my-8">
+                        <div className="flex">
+                            <div className="flex-shrink-0">
                                 {/* <!-- Heroicon name: solid/x-circle --> */}
-                                <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                <svg className="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
                                     aria-hidden="true">
-                                    <path fill-rule="evenodd"
+                                    <path fillRule="evenodd"
                                         d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                                        clip-rule="evenodd" />
+                                        clipRule="evenodd" />
                                 </svg>
                             </div>
-                            <div class="ml-3">
-                                <h3 class="text-sm font-medium text-red-800">A Github Application was not yet created by the installer. Go
-                                    to <a href="/" class="font-bold">Step One</a> to create it.</h3>
+                            <div className="ml-3">
+                                <h3 className="text-sm font-medium text-red-800">A Github Application was not yet created by the installer. Go
+                                    to <a href="/" className="font-bold">Step One</a> to create it.</h3>
                             </div>
                         </div>
                     </div>)
-                :
-                (<div class="text-sm">
-                    <h3 class="text-2xl font-bold pt-16">Your gitops repositories are now prepared</h3>
-                    <div class="pt-4">
-                        <p>
-                            Go checkout the repo for your infrastructure components: <br />
-                            👉 <a href={`https://github.com/${context.infraRepo}`} class="text-blue-600" rel="noreferrer" target="_blank">https://github.com/{context.infraRepo}</a>
-                        </p>
-                        <p class="mt-2">
-                            Don't forget to check the repo for your own applications: <br />
-                            👉 <a href={`https://github.com/${context.appsRepo}`} class="text-blue-600" rel="noreferrer" target="_blank">https://github.com/{context.appsRepo}</a>
-                        </p>
-                    </div>
-                    <h3 class="text-2xl font-bold pt-16">Kick off the gitops sync loop with the following steps</h3>
-                    <BootstrapGuide
-                        envName={context.envName}
-                        repoPath={context.infraRepo}
-                        repoPerEnv={context.repoPerEnv}
-                        publicKey={context.infraPublicKey}
-                        secretFileName={context.infraSecretFileName}
-                        gitopsRepoFileName={context.infraGitopsRepoFileName}
-                        isNewRepo={context.isNewInfraRepo}
-                    />
-                    <BootstrapGuide
-                        envName={context.envName}
-                        repoPath={context.appsRepo}
-                        repoPerEnv={context.repoPerEnv}
-                        publicKey={context.appsPublicKey}
-                        secretFileName={context.appsSecretFileName}
-                        gitopsRepoFileName={context.appsGitopsRepoFileName}
-                        isNewRepo={context.isNewAppsRepo}
-                    />
-                </div>)}
-                <div class="p-0 flow-root my-8">
-                    <span class="inline-flex rounded-md shadow-sm gap-x-3 float-right">
+                    :
+                    (<div className="text-sm">
+                        <h3 className="text-2xl font-bold pt-16">Your gitops repositories are now prepared</h3>
+                        <div className="pt-4">
+                            <p>
+                                Go checkout the repo for your infrastructure components: <br />
+                                👉 <a href={`https://github.com/${context.infraRepo}`} className="text-blue-600" rel="noreferrer" target="_blank">https://github.com/{context.infraRepo}</a>
+                            </p>
+                            <p className="mt-2">
+                                Don't forget to check the repo for your own applications: <br />
+                                👉 <a href={`https://github.com/${context.appsRepo}`} className="text-blue-600" rel="noreferrer" target="_blank">https://github.com/{context.appsRepo}</a>
+                            </p>
+                        </div>
+                        <h3 className="text-2xl font-bold pt-16">Kick off the gitops sync loop with the following steps</h3>
+                        <BootstrapGuide
+                            envName={context.envName}
+                            repoPath={context.infraRepo}
+                            repoPerEnv={context.repoPerEnv}
+                            publicKey={context.infraPublicKey}
+                            secretFileName={context.infraSecretFileName}
+                            gitopsRepoFileName={context.infraGitopsRepoFileName}
+                            isNewRepo={context.isNewInfraRepo}
+                        />
+                        <BootstrapGuide
+                            envName={context.envName}
+                            repoPath={context.appsRepo}
+                            repoPerEnv={context.repoPerEnv}
+                            publicKey={context.appsPublicKey}
+                            secretFileName={context.appsSecretFileName}
+                            gitopsRepoFileName={context.appsGitopsRepoFileName}
+                            isNewRepo={context.isNewAppsRepo}
+                        />
+                    </div>)}
+                <div className="p-0 flow-root my-8">
+                    <span className="inline-flex rounded-md shadow-sm gap-x-3 float-right">
                         <button
-                            class="bg-green-600 hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-indigo active:bg-green-700 inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white transition ease-in-out duration-150"
+                            className="bg-green-600 hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-indigo active:bg-green-700 inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white transition ease-in-out duration-150"
                             // eslint-disable-next-line no-restricted-globals
                             onClick={close()}>
                             I am done
