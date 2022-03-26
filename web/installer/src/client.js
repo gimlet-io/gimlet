d0a14158c3d1cfcd0b645fbaef15a4eb3444287c
szabogabor91 <86593865+szabogabor91@users.noreply.github.com>
2022-03-26 12:28:20 +0100

    Closing bootstrap page (#115)

---
+++ web/installer/src/client.js
@@ -0,0 +1,40 @@
+export default class GimletCLIClient {
+  constructor() {
+    if (typeof window !== 'undefined') {
+      let port = window.location.port
+      // if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+      //   port = 28000
+      // }
+      this.url = window.location.protocol + '//' + window.location.hostname
+      if (port && port !== '') {
+        this.url = this.url + ':' + port
+      }
+    }
+  }
+
+  URL() {
+    return this.url
+  }
+
+  post(path, body) {
+    fetch(this.url + path, {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then(response => {
+        if (!response.ok && window !== undefined) {
+          return Promise.reject({ status: response.status, statusText: response.statusText, path })
+        }
+        return response.json()
+      })
+      .catch((error) => {
+        this.onError(error)
+        throw error
+      })
+  }
+}
