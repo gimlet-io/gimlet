c71dbb08134c09a1b1bbf9d6ad3e7d375774087c
szabogabor91 <91szabogabor@gmail.com>
2022-04-06 14:16:05 +0200

    Add top and bot margin 4 to GimletD guidance card

--- web/dashboard/src/views/profile/profile.jsx
+++ web/dashboard/src/views/profile/profile.jsx
@@ -59,7 +59,7 @@ export default class Profile extends Component {
             <div className="px-4 py-8 sm:px-0">
               {githubAppSettings(this.state.application.name, this.state.application.appSettingsURL, this.state.application.installationURL)}
               {gimletdIntegrationEnabled &&
-                <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+                <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200 my-4">
                   <div className="px-4 py-5 sm:px-6">
                     <h3 className="text-lg leading-6 font-medium text-gray-900">
                       Gimlet CLI access
