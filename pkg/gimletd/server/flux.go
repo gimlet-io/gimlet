7259e0086e519175400f5c0920ea9d1eda00ac9c
Ádám <sisakadam@gmail.com>
2022-04-14 10:05:48 +0200

    Replaced time.now with the event timestamp

--- pkg/gimletd/server/flux.go
+++ pkg/gimletd/server/flux.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/fluxcd/pkg/runtime/events"
 	"github.com/gimlet-io/gimlet-cli/pkg/gimletd/model"
@@ -67,7 +66,7 @@ func asGitopsCommit(event events.Event) (*model.GitopsCommit, error) {
 		Sha:        sha,
 		Status:     event.Reason,
 		StatusDesc: statusDesc,
-		Created:    time.Now().Unix(),
+		Created:    event.Timestamp.Unix(),
 	}, nil
 }
 
