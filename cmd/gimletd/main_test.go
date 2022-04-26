26dfc90f4276cd8c321bc5555535d6e1db6b47c0
szabogabor91 <91szabogabor@gmail.com>
2022-04-26 12:44:16 +0200

    Move parseGitopsRepos test to main

--- cmd/gimletd/main_test.go
+++ cmd/gimletd/main_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gimlet-io/gimlet-cli/cmd/gimletd/config"
+	"gotest.tools/assert"
 )
 
 func TestParseChannelMapping(t *testing.T) {
@@ -24,3 +25,23 @@ func assertEqual(t *testing.T, a interface{}, b interface{}) {
 		t.Fatalf("%s != %s", a, b)
 	}
 }
+
+func TestParseGitopsRepos(t *testing.T) {
+	validInput := "env=staging&repoPerEnv=false&gitopsRepo=gitops-staging-infra&deployKeyPath=/deploykey/staging.key;env=production&repoPerEnv=true&gitopsRepo=gitops-production-infra&deployKeyPath=/deploykey/production.key"
+
+	gitopsRepos, err := parseGitopsRepos(validInput)
+	if err != nil {
+		t.Errorf("Cannot parse gitopsRepos")
+	}
+
+	assert.Equal(t, 2, len(gitopsRepos))
+	assert.Equal(t, "staging", gitopsRepos[0].Env)
+	assert.Equal(t, false, gitopsRepos[0].RepoPerEnv)
+	assert.Equal(t, "gitops-staging-infra", gitopsRepos[0].GitopsRepo)
+	assert.Equal(t, "/deploykey/staging.key", gitopsRepos[0].DeployKeyPath)
+	assert.Equal(t, "production", gitopsRepos[1].Env)
+	assert.Equal(t, true, gitopsRepos[1].RepoPerEnv)
+	assert.Equal(t, "gitops-production-infra", gitopsRepos[1].GitopsRepo)
+	assert.Equal(t, "/deploykey/production.key", gitopsRepos[1].DeployKeyPath)
+}
+
