f73b157729031942c35439c75715eeb3c1015dbd
Ádám <sisakadam@gmail.com>
2022-05-02 12:55:39 +0200

    Fixed env cards gitops commit list

--- web/dashboard/src/views/environments/environmentCard.jsx
+++ web/dashboard/src/views/environments/environmentCard.jsx
@@ -264,7 +264,7 @@ const EnvironmentCard = ({ store, isOnline, env, deleteEnv, gimletClient, refres
               <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
             </svg>
           </div>
-          {gitopsCommits.map((gitopsCommit, idx, arr) => renderGitopsCommit(gitopsCommit, idx, arr))}
+          {gitopsCommits.filter(gitopsCommit => gitopsCommit.env === env.name).map((gitopsCommit, idx, arr) => renderGitopsCommit(gitopsCommit, idx, arr))}
         </ul>
       </div>
     )
