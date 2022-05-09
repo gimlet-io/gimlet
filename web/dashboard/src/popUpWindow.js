d5aef2e6143ea8a81383dc1fba465f0e31a8f007
dzsak <86593861+dzsak@users.noreply.github.com>
2022-05-09 13:15:41 +0200

    Enhancement/close button to popup window (#149)

--- web/dashboard/src/popUpWindow.js
+++ web/dashboard/src/popUpWindow.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import { CheckIcon, XCircleIcon } from '@heroicons/react/solid'
+import {
+    ACTION_TYPE_POPUPWINDOWRESET
+} from "./redux/redux";
 
 export default class PopUpWindow extends Component {
     constructor(props) {
@@ -92,14 +95,29 @@ export default class PopUpWindow extends Component {
         }
     }
 
+    close() {
+        this.props.store.dispatch({
+            type: ACTION_TYPE_POPUPWINDOWRESET
+        });
+    }
+
     render() {
         return (
             <div
                 className={(this.state.popupWindow.visible ? "visible" : "invisible") + " fixed z-50 inset-0 flex px-4 py-6 pointer-events-none sm:p-6 w-full flex-col items-end space-y-4"}>
                 <div
                     className={this.backgroundColor() + " max-w-lg w-full text-gray-100 text-sm shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5"}>
-                    <div className="flex p-4">
+                    <div className="flex p-4 justify-between">
                         {this.savingText()}
+                        <div className="ml-4 flex-shrink-0 flex items-start">
+                            <button className="rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none"
+                                onClick={() => { this.close() }} >
+                                <span className="sr-only">Close</span>
+                                <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>)
