f5ee5c83ed9ce4b3eaae71b21e64ec827cd3346e
Ádám <sisakadam@gmail.com>
2022-07-04 11:39:25 +0200

    Fix deploy results check

--- web/dashboard/src/components/deployStatus/deployStatus.jsx
+++ web/dashboard/src/components/deployStatus/deployStatus.jsx
@@ -56,7 +56,7 @@ export default class DeployStatus extends Component {
     }
 
     // Feature 'Add results to deploy status', gitopsHashes will be deprecated!
-    if (!deploy.results) {
+    if (deploy.results) {
       if (deploy.status === 'processed') {
         gitopsWidget = (
           <div className="mt-2">
