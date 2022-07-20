ade569311f3137f2cbc58647d69a169613cb113f
Ádám <sisakadam@gmail.com>
2022-07-20 15:00:31 +0200

    Fix columns in envVarsTable

--- web/dashboard/src/views/envConfig/envVarsTable.jsx
+++ web/dashboard/src/views/envConfig/envVarsTable.jsx
@@ -19,18 +19,17 @@ export default function EnvVarsTable() {
             </div>
             <div className="mt-4 flex flex-col">
                 <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                        <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                    <div className="overflow-hidden inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                             <table className="min-w-full divide-y divide-gray-300">
                                 <thead className="bg-gray-50">
                                     <tr>
-                                        <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                        <th scope="col" className="py-3.5 pl-4 pr-3 text-sm font-semibold text-gray-900 sm:pl-6">
                                             Variable
                                         </th>
-                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        <th scope="col" className="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
                                             Type
                                         </th>
-                                        <th scope="col" className="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
+                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                             Value
                                         </th>
                                     </tr>
@@ -38,16 +37,15 @@ export default function EnvVarsTable() {
                                 <tbody className="bg-white">
                                     {variables.map((variable, variableIdx) => (
                                         <tr key={variable.variable} className={variableIdx % 2 === 0 ? undefined : 'bg-gray-50'}>
-                                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                            <td className="whitespace-normal py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                                                 {variable.variable}
                                             </td>
-                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{variable.type}</td>
-                                            <td className="relative whitespace-nowrap px-3 py-4 text-center text-sm text-gray-500">{variable.value}</td>
+                                            <td className="whitespace-normal px-16 py-4 text-sm text-gray-500">{variable.type}</td>
+                                            <td className="relative whitespace-normal p-4 px-3 py-4 text-left text-sm text-gray-500">{variable.value}</td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
-                        </div>
                     </div>
                 </div>
             </div>
