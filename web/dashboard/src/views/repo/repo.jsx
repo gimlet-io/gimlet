ce60e26ffb11b387280f7adf1e4006a0356898a7
Ádám Sisák <86593861+dzsak@users.noreply.github.com>
2022-07-21 16:13:12 +0200

    Early return if we dont have connected agents or envs (#234)
    * Early return if we dont have connected agents or envs
    
    * Misspell in comment
    
    * The truncate made the subdomain end with a dash, so we truncate then sanitize
    
    Co-authored-by: Laszlo Fogas <laszlo@laszlo.cloud>

--- web/dashboard/src/views/repo/repo.jsx
+++ web/dashboard/src/views/repo/repo.jsx
@@ -465,6 +465,10 @@ function stacks(connectedAgents, envName) {
 function envsForRepoFilteredBySearchFilter(envs, connectedAgents, repoName, searchFilter) {
   let filteredEnvs = {};
 
+  if (!connectedAgents || !envs) {
+    return filteredEnvs;
+  }
+  
   // iterate through all Kubernetes envs
   for (const env of envs) {
     filteredEnvs[env.name] = { name: env.name };
@@ -474,7 +478,7 @@ function envsForRepoFilteredBySearchFilter(envs, connectedAgents, repoName, sear
       return service.repo === repoName
     });
 
-    // applpy search box filter
+    // apply search box filter
     if (searchFilter !== '') {
       filteredEnvs[env.name].stacks = filteredEnvs[env.name].stacks.filter((service) => {
         return service.service.name.includes(searchFilter) ||
