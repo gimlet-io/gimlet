7dfa93a0b996007291fbc2bde5c657a5ba232290
Laszlo Fogas <laszlo@gimlet.io>
2022-09-20 09:51:17 +0200

    Stack is released

--- cmd/installer/main.go
+++ cmd/installer/main.go
@@ -74,8 +74,7 @@ func main() {
 	stackUrl := stack.DefaultStackURL
 	latestTag, _ := stack.LatestVersion(stackUrl)
 	if latestTag != "" {
-		// stackUrl = stackUrl + "?tag=" + latestTag
-		stackUrl = stackUrl + "?branch=main"
+		stackUrl = stackUrl + "?tag=" + latestTag
 	}
 
 	stackConfig := &dx.StackConfig{
