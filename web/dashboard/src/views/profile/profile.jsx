93b1df12ddaf81b14964ae7bc79397450e1427b3
Ádám <sisakadam@gmail.com>
2022-10-05 15:30:42 +0200

    Fix broken links

--- web/dashboard/src/views/profile/profile.jsx
+++ web/dashboard/src/views/profile/profile.jsx
@@ -221,7 +221,7 @@ function integrateGimletD() {
   return (
     // eslint-disable-next-line
     <a
-      href="https://gimlet.io/docs/configuring-gimletd/"
+      href="https://gimlet.io/docs/gimletd-configuration-reference"
       target="_blank"
       className="relative block w-full border-2 border-gray-300
               border-dashed rounded-lg p-12 text-center hover:border-gray-400
