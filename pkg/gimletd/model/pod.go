d47c1bcc72d826f7cf086bfc1b923bb49f1eb36f
Ádám <sisakadam@gmail.com>
2023-01-09 16:28:31 +0100

    Pod struct field changes

--- pkg/gimletd/model/pod.go
+++ pkg/gimletd/model/pod.go
@@ -2,8 +2,7 @@ package model
 
 type Pod struct {
 	ID         int64  `json:"-"  meddler:"id,pk"`
-	Name       string `json:"name,omitempty"  meddler:"name"`
-	Namespace  string `json:"deploymentNamespace,omitempty"  meddler:"deployment_namespace"`
+	Deployment string `json:"deployment,omitempty"  meddler:"deployment"`
 	Status     string `json:"status,omitempty"  meddler:"status"`
-	StatusDesc string `json:"statusDesc,omitempty"  meddler:"statusDesc"`
+	StatusDesc string `json:"statusDesc,omitempty"  meddler:"status_desc"`
 }
--- pkg/gimletd/store/pod.go
+++ pkg/gimletd/store/pod.go
@@ -2,7 +2,6 @@ package store
 
 import (
 	"database/sql"
-	"fmt"
 
 	"github.com/gimlet-io/gimlet-cli/pkg/gimletd/model"
 	queries "github.com/gimlet-io/gimlet-cli/pkg/gimletd/store/sql"
@@ -14,27 +13,23 @@ func (db *Store) Pod(deployment string) (*model.Pod, error) {
 	pod := new(model.Pod)
 	err := meddler.QueryRow(db, pod, stmt, deployment)
 
-	if err == sql.ErrNoRows {
-		return nil, nil
-	} else if err != nil {
-		return nil, err
-	}
-
 	return pod, err
 }
 
 func (db *Store) SaveOrUpdatePod(pod *model.Pod) error {
-	stmt := queries.Stmt(db.driver, queries.SelectPodByDeployment)
-	savedPod := new(model.Pod)
-	err := meddler.QueryRow(db, savedPod, stmt, fmt.Sprintf("%s/%s", pod.Namespace, pod.Name))
-	if err == sql.ErrNoRows {
-		return meddler.Insert(db, "pods", pod)
-	} else if err != nil {
-		return err
+	storedPod, err := db.Pod(pod.Deployment)
+
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return meddler.Insert(db, "pods", pod)
+		default:
+			return err
+		}
 	}
 
-	savedPod.Status = pod.Status
-	savedPod.StatusDesc = pod.StatusDesc
+	storedPod.Status = pod.Status
+	storedPod.StatusDesc = pod.StatusDesc
 
-	return meddler.Update(db, "pods", savedPod)
+	return meddler.Update(db, "pods", storedPod)
 }
