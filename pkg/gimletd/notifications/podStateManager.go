ec75bed866092a556211f77bb9147ddae2e2a965
Ádám <sisakadam@gmail.com>
2023-01-10 16:47:26 +0100

    TrackStates fix

--- pkg/gimletd/notifications/podStateManager.go
+++ pkg/gimletd/notifications/podStateManager.go
@@ -1,7 +1,6 @@
 package notifications
 
 import (
-	"database/sql"
 	"fmt"
 	"time"
 
@@ -14,51 +13,46 @@ import (
 type podStateManager struct {
 	notifManager Manager
 	store        store.Store
-	waitTime     int
+	waitTime     time.Duration
 }
 
-func NewPodStateManager(notifManager Manager, store store.Store, waitTime int) *podStateManager {
+func NewPodStateManager(notifManager Manager, store store.Store, waitTime time.Duration) *podStateManager {
 	return &podStateManager{notifManager: notifManager, store: store, waitTime: waitTime}
 }
 
 func (p podStateManager) Track(pods []api.Pod) {
-	p.trackStates(pods, p.store)
+	p.trackStates(pods)
 }
 
-func (p podStateManager) trackStates(pods []api.Pod, store store.Store) {
+func (p podStateManager) trackStates(pods []api.Pod) {
 	for _, pod := range pods {
-		deployment := fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
-		podFromStore, err := store.Pod(deployment)
-		if err == sql.ErrNoRows {
-			err = store.SaveOrUpdatePod(&model.Pod{
-				Name:       fmt.Sprintf("%s/%s", pod.Namespace, pod.Name),
-				Status:     pod.Status,
-				StatusDesc: pod.StatusDescription,
-			})
-			if err != nil {
-				logrus.Errorf("couldn't save or update pod: %s", err)
-				continue
-			}
-			if podErrorState(pod.Status) {
-				go p.checkWithDelay(store, pod)
-			}
-			continue
-		} else if err != nil {
-			logrus.Errorf("couldn't get pod from db: %s", err)
-			continue
-		}
+		podName := fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
+		currentTime := time.Now().Unix()
 
-		if podErrorState(pod.Status) && pod.Status != podFromStore.Status {
-			err = store.SaveOrUpdatePod(&model.Pod{
-				Name:       fmt.Sprintf("%s/%s", pod.Namespace, pod.Name),
-				Status:     pod.Status,
-				StatusDesc: pod.StatusDescription,
+		if podErrorState(pod.Status) {
+			err := p.store.SaveOrUpdatePod(&model.Pod{
+				Name:                podName,
+				Status:              pod.Status,
+				StatusDesc:          pod.StatusDescription,
+				AlertState:          "Pending",
+				AlertStateTimestamp: currentTime,
 			})
 			if err != nil {
-				logrus.Errorf("couldn't save or update pod: %s", err)
+				logrus.Errorf("could't save or update pod: %s", err)
 				continue
 			}
-			go p.checkWithDelay(store, pod)
+		} else {
+			err := p.store.SaveOrUpdatePod(&model.Pod{
+				Name:                podName,
+				Status:              pod.Status,
+				StatusDesc:          pod.StatusDescription,
+				AlertState:          "OK",
+				AlertStateTimestamp: currentTime,
+			})
+			if err != nil {
+				logrus.Errorf("could't save or update pod: %s", err)
+				continue
+			}
 		}
 	}
 }
