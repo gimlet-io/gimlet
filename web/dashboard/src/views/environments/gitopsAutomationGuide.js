3a4784016bdcc95c7a48f1a06f9ef02c2d705c1c
Laszlo Fogas <laszlo@gimlet.io>
2023-03-22 12:31:08 +0100

    Simplified bootstrap guide

--- web/dashboard/src/views/environments/gitopsAutomationGuide.js
+++ web/dashboard/src/views/environments/gitopsAutomationGuide.js
@@ -10,24 +10,19 @@ const GitopsAutomationGuide = () => {
                 <li>Flux uses the <code>`gitrepository`</code> custom resource to point to git repository locations and credentials. Flux's source controller periodically checks the content of the git repositories, and you can validate their status as follows:</li>
                 <CopiableCodeSnippet
                     color="blue"
-                    code={`kubectl get gitrepositories -A
-NAMESPACE     NAME                                       URL                                                        AGE    READY   STATUS
-flux-system   gitops-repo-gimlet-bootstraping-tutorial   ssh://git@github.com/gimlet/gimlet-bootstraping-tutorial   125m   True    stored artifact for revision 'main/f4a2a676bbcc04f38120b24463ca1c66cc099ab4'`}
+                    code={`kubectl get gitrepositories -A`}
                 />
                 <li>
                     If the git repositories are in ready state, validate the <code>`kustomization`</code> custom resources. These resources point to a path in a git repository to apply yamls from. If they are in ready state, you can be sure the Flux applied your latest manifests.
                 </li>
                 <CopiableCodeSnippet
                     color="blue"
-                    code={`kubectl get kustomizations -A 
-NAMESPACE     NAME                                                    AGE    READY   STATUS
-flux-system   gitops-repo-gimlet-bootstraping-tutorial                127m   True    Applied revision: main/f4a2a676bbcc04f38120b24463ca1c66cc099ab4
-flux-system   gitops-repo-gimlet-bootstraping-tutorial-dependencies   127m   True    Applied revision: main/f4a2a676bbcc04f38120b24463ca1c66cc099ab4`}
+                    code={`kubectl get kustomizations -A`}
                 />
                 <li>
                     Now that the gitops automation is in place, every manifest you put in the gitops repositories will be applied on the cluster by the gitops controller.
                 </li>
-                <li className="text-lg font-bold text-blue-800 pt-8">Need to debug Flux?</li>
+                <li className="text-lg font-bold text-blue-800">Need to debug Flux?</li>
                 <li>If <code>`kustomizations`</code> or <code>`gitrepositories`</code> are not in ready state, you can see the error message if you run `kubectl describe` on them.</li>
                 <li>If you need to further debug their behavior, you can check Flux logs in the <code>`flux-system`</code> namespace.</li>
                 <CopiableCodeSnippet
--- web/installer/src/gitopsAutomationGuide.js
+++ web/installer/src/gitopsAutomationGuide.js
@@ -8,23 +8,18 @@ const GitopsAutomationGuide = () => {
                 <li>Check Flux's custom resources on the cluster to verify the gitops automation.</li>
                 <li>Flux uses the <code>`gitrepository`</code> custom resource to point to git repository locations and credentials. Flux's source controller periodically checks the content of the git repositories, and you can validate their status as follows:</li>
                 <code className="block whitespace-pre overflow-x-scroll font-mono text-xs my-4 p-2 bg-blue-100 text-blue-500 rounded">
-                    {`kubectl get gitrepositories -A
-NAMESPACE     NAME                                       URL                                                        AGE    READY   STATUS
-flux-system   gitops-repo-gimlet-bootstraping-tutorial   ssh://git@github.com/gimlet/gimlet-bootstraping-tutorial   125m   True    stored artifact for revision 'main/f4a2a676bbcc04f38120b24463ca1c66cc099ab4'`}
+                    {`kubectl get gitrepositories -A`}
                 </code>
                 <li>
                     If the git repositories are in ready state, validate the <code>`kustomization`</code> custom resources. These resources point to a path in a git repository to apply yamls from. If they are in ready state, you can be sure the Flux applied your latest manifests.
                 </li>
                 <code className="block whitespace-pre overflow-x-scroll font-mono text-xs my-4 p-2 bg-blue-100 text-blue-500 rounded">
-                    {`kubectl get kustomizations -A 
-NAMESPACE     NAME                                                    AGE    READY   STATUS
-flux-system   gitops-repo-gimlet-bootstraping-tutorial                127m   True    Applied revision: main/f4a2a676bbcc04f38120b24463ca1c66cc099ab4
-flux-system   gitops-repo-gimlet-bootstraping-tutorial-dependencies   127m   True    Applied revision: main/f4a2a676bbcc04f38120b24463ca1c66cc099ab4`}
+                    {`kubectl get kustomizations -A`}
                 </code>
                 <li>
                     Now that the gitops automation is in place, every manifest you put in the gitops repositories will be applied on the cluster by the gitops controller.
                 </li>
-                <li className="text-lg font-bold text-blue-800 pt-8">Need to debug Flux?</li>
+                <li className="text-lg font-bold text-blue-800">Need to debug Flux?</li>
                 <li>If <code>`kustomizations`</code> or <code>`gitrepositories`</code> are not in ready state, you can see the error message if you run `kubectl describe` on them.</li>
                 <li>If you need to further debug their behavior, you can check Flux logs in the <code>`flux-system`</code> namespace.</li>
                 <code className="block whitespace-pre overflow-x-scroll font-mono text-xs my-4 p-2 bg-blue-100 text-blue-500 rounded">
