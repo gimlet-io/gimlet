ffe10d1a1f7591dfbfb54ac59ce130addfc38b2d
Laszlo Fogas <laszlo@gimlet.io>
2023-03-29 12:06:03 +0200

    Fixes

--- web/installer/src/stepTwo.jsx
+++ web/installer/src/stepTwo.jsx
@@ -1,18 +1,7 @@
 import { useEffect, useState } from 'react';
 import SeparateEnvironments from './separateEnvironments';
 
-const StepTwo = ({ getContext }) => {
-  const [context, setContext] = useState({
-    stackConfig: {
-      config: {
-        gimletd: {
-          environments: [{}]
-        },
-        gimletAgent: {},
-      }
-    }
-  });
-
+const StepTwo = () => {
   const [env, setEnv] = useState('test');
   const [repoPerEnv, setRepoPerEnv] = useState(true);
   const [infra, setInfra] = useState('gitops-test-infra');
@@ -27,10 +16,6 @@ const StepTwo = ({ getContext }) => {
       setApps(`gitops-apps`);
     }
   }, [repoPerEnv, env]);
-
-  if (!context) {
-    return null;
-  }
 
   return (
     <div className="mt-32 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -102,25 +87,6 @@ const StepTwo = ({ getContext }) => {
           </ol>
         </nav>
 
-        {context.appId === "" &&
-          <div className="rounded-md bg-red-50 p-4 my-8">
-            <div className="flex">
-              <div className="flex-shrink-0">
-                {/* <!-- Heroicon name: solid/x-circle --> */}
-                <svg className="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                  aria-hidden="true">
-                  <path fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                    clipRule="evenodd" />
-                </svg>
-              </div>
-              <div className="ml-3">
-                <h3 className="text-sm font-medium text-red-800">A Github Application was not yet created by the installer. Go
-                  to <a href="/" className="font-bold">Step One</a> to create it.</h3>
-              </div>
-            </div>
-          </div>
-        }
         <form action="/bootstrap" method="post">
           <div className="mt-8 text-sm">
 
