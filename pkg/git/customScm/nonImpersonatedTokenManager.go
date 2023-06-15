ff9e4a5d553e9f09a370fbcbc63d9d9dbfb614c2
Laszlo Fogas <laszlo@gimlet.io>
2023-06-15 12:28:44 +0200

    Reverting chagnes

--- pkg/git/customScm/nonImpersonatedTokenManager.go
+++ pkg/git/customScm/nonImpersonatedTokenManager.go
@@ -4,18 +4,3 @@ type NonImpersonatedTokenManager interface {
 	Token() (string, string, error)
 	AppToken() (string, error)
 }
-
-type DummyTokenManager struct {
-}
-
-func NewDummyTokenManager() *DummyTokenManager {
-	return &DummyTokenManager{}
-}
-
-func (t *DummyTokenManager) Token() (string, string, error) {
-	return "", "", nil
-}
-
-func (t *DummyTokenManager) AppToken() (string, error) {
-	return "", nil
-}
