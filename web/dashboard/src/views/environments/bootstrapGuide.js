1e42fdbb2f332b4654f79d365378120e5d6fd8ab
Laszlo Fogas <laszlo@gimlet.io>
2023-08-07 14:23:02 +0200

    Cluster start command

--- web/dashboard/src/views/environments/bootstrapGuide.js
+++ web/dashboard/src/views/environments/bootstrapGuide.js
@@ -7,6 +7,16 @@ const BootstrapGuide = ({ envName, token }) => {
 
     return (
       <>
+      <li>👉 Need a cluster? <a className='underline' target='_blank' rel="noopener noreferrer" href="https://gimlet.io/blog/running-kubernetes-on-your-laptop-with-k3d">We recommend using k3d</a> on your laptop if you are evaluating Gimlet.
+      <br/>But any Kubernetes cluster will do. Skip this step if you have one.<br />
+      <br/>Optional - Run the following commands to get a containerized Kubernetes cluster on your laptop:
+                <CopiableCodeSnippet
+          copiable
+          color="blue"
+          code={`curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d cluster create gimlet-cluster --k3s-arg "--disable=traefik@server:0"`}
+        />
+                </li>
         <li>👉 Install Gimlet CLI</li>
         <CopiableCodeSnippet
           copiable
@@ -30,7 +40,7 @@ sudo mv ./gimlet /usr/local/bin/gimlet`}
   };
 
   return (
-    <div className="rounded-md bg-blue-50 p-4 mb-4 overflow-hidden">
+    <div className="p-4 mb-4 overflow-hidden">
       <ul className="break-all text-sm text-blue-700 space-y-2">
         {renderBootstrapGuideText()}
       </ul>
