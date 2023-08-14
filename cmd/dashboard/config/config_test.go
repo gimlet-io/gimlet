ac4c5f83de9d0b6a6de74327570cffb61854a7d3
Laszlo Fogas <laszlo@gimlet.io>
2023-08-14 14:55:21 +0200

    Making flag configurable

---
+++ cmd/dashboard/config/config_test.go
@@ -0,0 +1,39 @@
+package config
+
+import "testing"
+
+func TestBuiltInEnvFlag(t *testing.T) {
+	c := &Config{}
+	defaults(c)
+	flag := c.BuiltinEnvFeatureFlag()
+	if !flag {
+		t.Errorf("FEATURE_BUILT_IN_ENV should default to true")
+	}
+
+	c = &Config{
+		BuiltinEnvFeatureFlagString: "false",
+	}
+	defaults(c)
+	flag = c.BuiltinEnvFeatureFlag()
+	if flag {
+		t.Errorf("FEATURE_BUILT_IN_ENV should accept values")
+	}
+
+	c = &Config{
+		BuiltinEnvFeatureFlagString: "true",
+	}
+	defaults(c)
+	flag = c.BuiltinEnvFeatureFlag()
+	if !flag {
+		t.Errorf("FEATURE_BUILT_IN_ENV should accept values")
+	}
+
+	c = &Config{
+		BuiltinEnvFeatureFlagString: "not a boolean",
+	}
+	defaults(c)
+	flag = c.BuiltinEnvFeatureFlag()
+	if !flag {
+		t.Errorf("FEATURE_BUILT_IN_ENV should default to true in case of parse errors")
+	}
+}
