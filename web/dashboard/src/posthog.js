cef1353693475ef1adf9c176c6365c1ab9d02cec
dzsak <sisakadam@gmail.com>
2023-08-23 09:54:39 +0200

    Posthog config settings

--- web/dashboard/src/posthog.js
+++ web/dashboard/src/posthog.js
@@ -17,40 +17,31 @@ const Posthog = ({ store }) => {
   });
 
   useEffect(() => {
-    if (posthogFeatureFlag === undefined) {
-      return;
-    }
-    if ( posthogIdentifyUser === undefined) {
-      return;
-    }
-    if(!posthogApiKey) {
-      return;
-    }
     if (!posthogFeatureFlag) {
       return;
     }
+    if (posthogIdentifyUser === undefined) {
+      return;
+    }
 
-    // TODO
-    // hide sensitives from autocapture events
-    // (clicked p with text "owner/repo"
-    // url/screen http://localhost:9000/repo/owner/repo/envs/staging-0/config/onechart-4e96/new
-    // Pageleave http://localhost:9000/repo/owner/repo/envs/staging-0/config/onechart-570a/new)
-    if (!posthog.__loaded) {
+    if (!posthog.__loaded && posthogApiKey) {
       posthog.init(posthogApiKey, {
         api_host: 'https://eu.posthog.com',
-        disable_session_recording: false, // TODO disable on open source usage
+        disable_session_recording: !posthogIdentifyUser, // TODO disable on open source usage
+        enable_recording_console_log: false,
         session_recording: {
           maskTextSelector: "*" // Masks all text elements
         },
+        capture_pageview: false,
+        capture_pageleave: false,
         autocapture: {
-          url_allowlist: [/\/repositories/, /\/environments/, /\/settings/, /\/profile/], // strings or RegExps
-          element_allowlist: ['a', 'button', 'form', 'input', 'select', 'textarea', 'label'], // DOM elements from this list ['a', 'button', 'form', 'input', 'select', 'textarea', 'label']
+          url_allowlist: [/\/repositories/, /\/environments/, /\/settings/, /\/profile/],
+          element_allowlist: ['a', 'button', 'form', 'input', 'select', 'textarea', 'label'],
         },
       });
-      console.log(posthog.config)
     }
 
-    if (posthog.__loaded && posthogIdentifyUser && login) {
+    if (posthogIdentifyUser && posthog.__loaded && login) {
       posthog.identify(login)
     }
   }, [posthogFeatureFlag, posthogApiKey, posthogIdentifyUser, login]);
