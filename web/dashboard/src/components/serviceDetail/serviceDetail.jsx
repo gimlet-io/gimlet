9ade8cec7ff0d2fd0020fc9c8a4e6b91fca0407f
Ádám <sisakadam@gmail.com>
2023-08-24 17:01:35 +0200

    Pod logs color

--- web/dashboard/src/components/serviceDetail/serviceDetail.jsx
+++ web/dashboard/src/components/serviceDetail/serviceDetail.jsx
@@ -328,7 +328,7 @@ const PodLogsOverlay = ({ visible, namespace, svc, closeLogsOverlayHandler, stor
             </button>
           </div>
           <div className="h-full relative overflow-y-auto p-4 bg-slate-800 rounded-lg">
-            {logs?.map((line, idx) => <p key={idx} className='font-mono text-xs text-yellow-200'>{line.content}</p>)}
+            {logs?.map((line, idx) => <p key={idx} className={`font-mono text-xs ${line.color}`}>{line.content}</p>)}
             <p ref={logsEndRef} />
           </div>
         </div>
