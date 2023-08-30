fbaa28a723f9cad9a11bdf449ac55291eb6294b2
Laszlo Fogas <laszlo@gimlet.io>
2023-08-30 19:42:24 +0200

    Always display a dropdown

--- web/dashboard/src/components/deployWidget/deployWidget.jsx
+++ web/dashboard/src/components/deployWidget/deployWidget.jsx
@@ -42,26 +42,6 @@ export default function DeployWidget(props) {
       return null
     }
 
-    if (targets.length === 1) {
-      return (
-        // eslint-disable-next-line
-        <button
-          type="button"
-          onClick={() => {
-            posthog?.capture('Magic deploy button pushed')
-            magicDeployHandler(targets[0].env, targets[0].app, repo, sha)
-          }}
-          className="inline-flex items-center gap-x-1.5 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-slate-800"
-        >
-          <img
-            className="h-5 w-auto" src={logo} alt="Deploy"/>
-          <span
-            className="bg-gradient-to-r from-orange-400 from-0% via-pink-400 via-40% to-pink-500 to-90% text-transparent bg-clip-text">
-            Deploy
-          </span>
-        </button>
-      )
-    } else {
     return (
       <span className="relative inline-flex flex-row rounded-md">
         <Menu as="span" className="relative inline-flex shadow-sm rounded-md align-middle">
@@ -110,8 +90,7 @@ export default function DeployWidget(props) {
           </span>
         </Menu>
       </span>
-      )
-    }
+    )
   }
 
   let deployTargetsByEnv = {};
