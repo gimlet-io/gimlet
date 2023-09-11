9e03190fe5e2c35733f6e3d07dc3e08b9c2ea256
Ádám <sisakadam@gmail.com>
2023-09-11 16:29:39 +0200

    Repo view refresh; Tenant selector positioning

--- web/dashboard/src/views/repo/tenantSelector.jsx
+++ web/dashboard/src/views/repo/tenantSelector.jsx
@@ -7,7 +7,7 @@ const TenantSelector = ({ tenants, selectedTenant, setSelectedTenant }) => {
   }
 
   return (
-    <Menu as="span" className="relative inline-flex shadow-sm rounded-md align-middle mt-8 sm:mt-0">
+    <Menu as="span" className="relative inline-flex shadow-sm rounded-md align-middle mt-8 sm:mt-4">
       <Menu.Button
         className="relative cursor-pointer inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700"
       >
@@ -22,7 +22,7 @@ const TenantSelector = ({ tenants, selectedTenant, setSelectedTenant }) => {
           <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
         </Menu.Button>
         <Menu.Items
-          className="origin-top-right absolute z-50 sm:right-0 mt-2 -mr-1 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          className="origin-top-right absolute z-50 mt-2 -mr-1 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item key="all">
               {({ active }) => (
