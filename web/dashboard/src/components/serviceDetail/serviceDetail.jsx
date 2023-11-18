82748b1becbf43f5d0f52d1a71d5510477cd2289
Ádám Sisák <86593861+dzsak@users.noreply.github.com>
2023-11-18 07:59:12 +0100

    Open osca links in a new tab (#713)

--- web/dashboard/src/components/serviceDetail/serviceDetail.jsx
+++ web/dashboard/src/components/serviceDetail/serviceDetail.jsx
@@ -241,30 +241,32 @@ function ServiceDetail(props) {
                   </p>
                 </div>
                 }
-                <div>
-                  <p className="text-base text-gray-600">Links</p>
-                  <div className="text-gray-700 text-sm mt-2">
-                    {stack.osca &&
-                      Object.keys(stack.osca.links).map((k, idx, ar) => {
-                        return (
-                          <>
-                            <a href={stack.osca.links[k]} className="capitalize">
-                              {k}
-                              <svg xmlns="http://www.w3.org/2000/svg"
-                                className="inline fill-current text-gray-500 hover:text-gray-700 h-4 w-4"
-                                viewBox="0 0 24 24">
-                                <path d="M0 0h24v24H0z" fill="none" />
-                                <path
-                                  d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
-                              </svg>
-                            </a>
-                            {idx !== ar.length - 1 && <span className="px-2">|</span>}
-                          </>
-                        )
-                      })
-                    }
+                {stack.osca && Object.keys(stack.osca.links).length !== 0 &&
+                  <div>
+                    <p className="text-base text-gray-600">Links</p>
+                    <div className="text-gray-700 text-sm mt-2">
+                      {
+                        Object.keys(stack.osca.links).map((k, idx, ar) => {
+                          return (
+                            <div key={k}>
+                              <a href={stack.osca.links[k]} rel="noreferrer" target="_blank" className="capitalize">
+                                {k}
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                  className="inline fill-current text-gray-500 hover:text-gray-700 h-4 w-4"
+                                  viewBox="0 0 24 24">
+                                  <path d="M0 0h24v24H0z" fill="none" />
+                                  <path
+                                    d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+                                </svg>
+                              </a>
+                              {idx !== ar.length - 1 && <span className="px-2">|</span>}
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
                   </div>
-                </div>
+                }
               </div>
               <div className="col-span-7 space-y-4 pl-2">
                 { deployment &&
