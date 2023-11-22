640a75ef2a9f3bb49f690ff8f93e7c9ee2a94ff0
Ádám <sisakadam@gmail.com>
2023-11-22 16:43:49 +0100

    Port forward url

--- web/dashboard/src/components/serviceDetail/serviceDetail.jsx
+++ web/dashboard/src/components/serviceDetail/serviceDetail.jsx
@@ -305,6 +305,20 @@ function ServiceDetail(props) {
                       </p>
                       ) : null
                     }
+                    {config &&
+                        <>
+                          <a href={'http://127.0.0.1:' + hostPort} target="_blank" rel="noopener noreferrer">http://127.0.0.1:{hostPort}
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                              className="inline fill-current text-gray-500 hover:text-gray-700 mr-1 h-4 w-4"
+                              viewBox="0 0 24 24">
+                              <path d="M0 0h24v24H0z" fill="none" />
+                              <path
+                                d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+                            </svg>
+                          </a>
+                          (port-forward)
+                        </>
+                      }
                   </div>
                 </div>
                 }
