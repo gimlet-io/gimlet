7f551c1ac34f50fe97e1c57205b61206cae56df0
Laszlo Fogas <laszlo@gimlet.io>
2024-03-01 16:19:21 +0100

    Showing commitevents

--- web/dashboard/src/components/commits/commits.js
+++ web/dashboard/src/components/commits/commits.js
@@ -5,6 +5,7 @@ import DeployWidget from "../deployWidget/deployWidget";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { ACTION_TYPE_UPDATE_COMMITS } from "../../redux/redux";
 import { Modal, SkeletonLoader } from "./modal";
+import { CheckIcon, ThumbUpIcon, UserIcon } from '@heroicons/react/solid'
 
 const Commits = ({ commits, envs, connectedAgents, deployHandler, owner, repo, gimletClient, store, branch, scmUrl, tenant }) => {
   const [isScrollButtonActive, setIsScrollButtonActive] = useState(false)
@@ -127,7 +128,7 @@ const CommitWidget = ({ owner, repo, repoName, commit, last, idx, commitsRef, en
           closeHandler={() => setShowModal(false)}
         >
           {events ?
-            <span>loaded</span>
+            <CommitEvents events={events} />
             :
             <SkeletonLoader />
           }
@@ -231,6 +232,115 @@ const filterDeployTargets = (deployTargets, envs, tenant) => {
 
   return filteredTargets;
 };
+
+function CommitEvents(props) {
+  const { events } = props
+
+  const timeline = [
+
+
+    {
+      id: 1,
+      content: 'Applied to',
+      target: 'Front End Developer',
+      href: '#',
+      date: 'Sep 20',
+      datetime: '2020-09-20',
+      icon: UserIcon,
+      iconBackground: 'bg-gray-400',
+    },
+    {
+      id: 2,
+      content: 'Advanced to phone screening by',
+      target: 'Bethany Blake',
+      href: '#',
+      date: 'Sep 22',
+      datetime: '2020-09-22',
+      icon: ThumbUpIcon,
+      iconBackground: 'bg-blue-500',
+    },
+    {
+      id: 3,
+      content: 'Completed phone screening with',
+      target: 'Martha Gardner',
+      href: '#',
+      date: 'Sep 28',
+      datetime: '2020-09-28',
+      icon: CheckIcon,
+      iconBackground: 'bg-green-500',
+    },
+    {
+      id: 4,
+      content: 'Advanced to interview by',
+      target: 'Bethany Blake',
+      href: '#',
+      date: 'Sep 30',
+      datetime: '2020-09-30',
+      icon: ThumbUpIcon,
+      iconBackground: 'bg-blue-500',
+    },
+    {
+      id: 5,
+      content: 'Completed interview with',
+      target: 'Katherine Snyder',
+      href: '#',
+      date: 'Oct 4',
+      datetime: '2020-10-04',
+      icon: CheckIcon,
+      iconBackground: 'bg-green-500',
+    },
+  ]
+  
+
+  return (
+    <div>
+      <div className="flow-root">
+        <ul role="list" className="-mb-8">
+          {events.map((event, eventIdx) => (
+            <li key={event.created}>
+              <div className="relative pb-8">
+                {eventIdx !== timeline.length - 1 ? (
+                  <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+                ) : null}
+                <div className="relative flex space-x-3">
+                  <div>
+                    <span
+                      className={classNames(
+                        event.type === "release" ? 'bg-green-500' : 'bg-blue-500',
+                        'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'
+                      )}
+                    >
+                      {event.type === "release" &&
+                      <ThumbUpIcon className="h-5 w-5 text-white" aria-hidden="true" />
+                      }
+                      {event.type !== "release" &&
+                      <CheckIcon className="h-5 w-5 text-white" aria-hidden="true" />
+                      }
+                    </span>
+                  </div>
+                  <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                    <div>
+                      <p className="text-sm text-gray-500">
+                        {event.type}{' '}{event.status}
+                      </p>
+                    </div>
+                    <div className="whitespace-nowrap text-right text-sm text-gray-500">
+                      <time dateTime={event.created}>{event.created}</time>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default Commits;
 
