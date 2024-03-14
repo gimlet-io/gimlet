99f1096c475131e63330a3f14d11a11bd891415c
dzsak <sisakadam@gmail.com>
2024-03-14 16:57:18 +0100

    Seal logic

--- web/dashboard/src/views/environment/registryWidgets.jsx
+++ web/dashboard/src/views/environment/registryWidgets.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { ACTION_TYPE_POPUPWINDOWERROR } from "../../redux/redux";
 
 export class GhcrRegistryWidget extends Component {
   constructor(props) {
@@ -12,6 +13,8 @@ export class GhcrRegistryWidget extends Component {
 
   seal() {
     const { email, login, token } = this.state;
+    const { gimletClient, store, env } = this.props;
+
     const configjson = {
       "auths": {
         "ghcr.io": {
@@ -20,25 +23,35 @@ export class GhcrRegistryWidget extends Component {
         }
       }
     }
-    const configjsonString = JSON.stringify(configjson)
+
     return () => {
-      this.props.onChange("toSeal: " + configjsonString)
+      gimletClient.seal(env, JSON.stringify(configjson))
+        .then(data => {
+          this.props.onChange(data)
+        }, () => {
+          store.dispatch({
+            type: ACTION_TYPE_POPUPWINDOWERROR, payload: {
+              header: "Error",
+              message: "Failed to seal."
+            }
+          });
+          this.resetPopupWindowAfterThreeSeconds()
+        });
     };
   }
 
   render() {
     return (
       <>
-        <div class="form-group field field-string">
-          <label class="control-label" for="root_login">Login</label>
-          <input class="form-control" id="root_login" required="" label="Login" placeholder="" type="text" list="examples_root_login"
-            value={this.state.login} onChange={e => this.setState({ login: e.target.value })} />
-        </div>
-        <div class="form-group field field-string">
-          <label class="control-label" for="root_token">Token</label>
-          <input class="form-control" id="root_token" required="" label="Token" placeholder="" type="text" list="examples_root_token"
-            value={this.state.token} onChange={e => this.setState({ token: e.target.value })} />
-        </div>
+
+        <label class="control-label" for="root_login">Login</label>
+        <input class="form-control" id="root_login" required="" label="Login" placeholder="" type="text" list="examples_root_login"
+          value={this.state.login} onChange={e => this.setState({ login: e.target.value })} />
+
+        <label class="control-label" for="root_token">Token</label>
+        <input class="form-control" id="root_token" required="" label="Token" placeholder="" type="text" list="examples_root_token"
+          value={this.state.token} onChange={e => this.setState({ token: e.target.value })} />
+
         <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-12"
           onClick={this.seal()} >
           Seal
@@ -61,6 +74,8 @@ export class DockerhubRegistryWidget extends Component {
 
   seal() {
     const { email, login, token } = this.state;
+    const { gimletClient, store, env } = this.props;
+
     const configjson = {
       "auths": {
         "https://index.docker.io/v1/": {
@@ -69,30 +84,39 @@ export class DockerhubRegistryWidget extends Component {
         }
       }
     }
-    const configjsonString = JSON.stringify(configjson)
+
     return () => {
-      this.props.onChange("toSeal: " + configjsonString)
+      gimletClient.seal(env, JSON.stringify(configjson))
+        .then(data => {
+          this.props.onChange(data)
+        }, () => {
+          store.dispatch({
+            type: ACTION_TYPE_POPUPWINDOWERROR, payload: {
+              header: "Error",
+              message: "Failed to seal."
+            }
+          });
+          this.resetPopupWindowAfterThreeSeconds()
+        });
     };
   }
 
   render() {
     return (
       <>
-        <div class="form-group field field-string">
-          <label class="control-label" for="root_email">Email</label>
-          <input class="form-control" id="root_email" required="" label="Email" placeholder="" type="text" list="examples_root_email"
-            value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
-        </div>
-        <div class="form-group field field-string">
-          <label class="control-label" for="root_login">Login</label>
-          <input class="form-control" id="root_login" required="" label="Login" placeholder="" type="text" list="examples_root_login"
-            value={this.state.login} onChange={e => this.setState({ login: e.target.value })} />
-        </div>
-        <div class="form-group field field-string">
-          <label class="control-label" for="root_token">Token</label>
-          <input class="form-control" id="root_token" required="" label="Token" placeholder="" type="text" list="examples_root_token"
-            value={this.state.token} onChange={e => this.setState({ token: e.target.value })} />
-        </div>
+
+        <label class="control-label" for="root_email">Email</label>
+        <input class="form-control" id="root_email" required="" label="Email" placeholder="" type="text" list="examples_root_email"
+          value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
+
+        <label class="control-label" for="root_login">Login</label>
+        <input class="form-control" id="root_login" required="" label="Login" placeholder="" type="text" list="examples_root_login"
+          value={this.state.login} onChange={e => this.setState({ login: e.target.value })} />
+
+        <label class="control-label" for="root_token">Token</label>
+        <input class="form-control" id="root_token" required="" label="Token" placeholder="" type="text" list="examples_root_token"
+          value={this.state.token} onChange={e => this.setState({ token: e.target.value })} />
+
         <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-12"
           onClick={this.seal()} >
           Seal
