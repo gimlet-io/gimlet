213e3c000b9362dfa78bd6d232a656db954ff1b6
dzsak <sisakadam@gmail.com>
2024-03-15 13:31:54 +0100

    Popup reset

--- web/dashboard/src/views/environment/registryWidgets.jsx
+++ web/dashboard/src/views/environment/registryWidgets.jsx
@@ -1,5 +1,8 @@
 import { Component } from "react";
-import { ACTION_TYPE_POPUPWINDOWERROR } from "../../redux/redux";
+import {
+  ACTION_TYPE_POPUPWINDOWRESET,
+  ACTION_TYPE_POPUPWINDOWERROR
+ } from "../../redux/redux";
 
 export class GhcrRegistryWidget extends Component {
   constructor(props) {
@@ -10,6 +13,15 @@ export class GhcrRegistryWidget extends Component {
       token: "",
     };
   }
+
+  resetPopupWindowAfterThreeSeconds() {
+    const { store } = this.props;
+    setTimeout(() => {
+      store.dispatch({
+        type: ACTION_TYPE_POPUPWINDOWRESET
+      });
+    }, 3000);
+  };
 
   seal() {
     const { email, login, token } = this.state;
@@ -43,16 +55,13 @@ export class GhcrRegistryWidget extends Component {
   render() {
     return (
       <>
-
         <label class="control-label" for="root_login">Login</label>
         <input class="form-control" id="root_login" required="" label="Login" placeholder="" type="text" list="examples_root_login"
           value={this.state.login} onChange={e => this.setState({ login: e.target.value })} />
-
         <label class="control-label" for="root_token">Token</label>
         <input class="form-control" id="root_token" required="" label="Token" placeholder="" type="text" list="examples_root_token"
           value={this.state.token} onChange={e => this.setState({ token: e.target.value })} />
-
-        <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-12"
+        <button className="my-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-12"
           onClick={this.seal()} >
           Seal
         </button>
@@ -71,6 +80,15 @@ export class DockerhubRegistryWidget extends Component {
       token: "",
     };
   }
+
+  resetPopupWindowAfterThreeSeconds() {
+    const { store } = this.props;
+    setTimeout(() => {
+      store.dispatch({
+        type: ACTION_TYPE_POPUPWINDOWRESET
+      });
+    }, 3000);
+  };
 
   seal() {
     const { email, login, token } = this.state;
@@ -104,20 +122,16 @@ export class DockerhubRegistryWidget extends Component {
   render() {
     return (
       <>
-
         <label class="control-label" for="root_email">Email</label>
         <input class="form-control" id="root_email" required="" label="Email" placeholder="" type="text" list="examples_root_email"
           value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
-
         <label class="control-label" for="root_login">Login</label>
         <input class="form-control" id="root_login" required="" label="Login" placeholder="" type="text" list="examples_root_login"
           value={this.state.login} onChange={e => this.setState({ login: e.target.value })} />
-
         <label class="control-label" for="root_token">Token</label>
         <input class="form-control" id="root_token" required="" label="Token" placeholder="" type="text" list="examples_root_token"
           value={this.state.token} onChange={e => this.setState({ token: e.target.value })} />
-
-        <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-12"
+        <button className="my-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-12"
           onClick={this.seal()} >
           Seal
         </button>
