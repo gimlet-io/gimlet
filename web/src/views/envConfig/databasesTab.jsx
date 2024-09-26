c032c416a0745d93b34d49c9308a6c16c6b408ad
Laszlo Fogas <laszlo@gimlet.io>
2024-09-26 20:34:33 +0200

    combobox with module title

--- web/src/views/envConfig/databasesTab.jsx
+++ web/src/views/envConfig/databasesTab.jsx
@@ -66,7 +66,7 @@ export default function ModuleSelector(props) {
     }
   })
   const [query, setQuery] = useState('')
-  const [selectedPerson, setSelectedPerson] = useState(null)
+  const [selected, setSelected] = useState(parsedModules[0].schema.title)
 
   const filteredModules =
     query === ''
@@ -78,19 +78,18 @@ export default function ModuleSelector(props) {
   return (
     <Combobox
       as="div"
-      value={selectedPerson}
-      onChange={(person) => {
+      value={selected}
+      onChange={(moduleTitle) => {
         setQuery('')
-        setSelectedPerson(person)
+        setSelected(moduleTitle)
       }}
     >
-      <Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Label>
       <div className="relative mt-2">
         <ComboboxInput
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
           onBlur={() => setQuery('')}
-          displayValue={(person) => person?.name}
+          displayValue={selected}
         />
         <ComboboxButton className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
